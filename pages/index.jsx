507e42a4e215584cc8028b06ebf8c2e5bea91a36
cuginoAle <alessio.carnevale@gmail.com>
2022-12-23 19:19:34 +0100

    wip

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,31 @@
 import { Component } from 'react';
-import { attributes, react as HomeContent } from 'content/home.md';
+import Image from 'next/image';
+// import { attributes, react as HomeContent } from '_data/home.md';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 export default class Home extends Component {
   render() {
-    let { title, cats } = attributes;
+    const { intro, picture } = JSON.parse(this.props.data);
+
     return (
       <article>
-        <h1>{title}</h1>
-        <HomeContent />
-        <ul>
-          {cats.map((cat, k) => (
-            <li key={k}>
-              <h2>{cat.name}</h2>
-              <p>{cat.description}</p>
-            </li>
-          ))}
-        </ul>
+        <h1>Bau</h1>
+        {/* <HomeContent /> */}
+        <ReactMarkdown remarkPlugins={[remarkGfm]}>{intro}</ReactMarkdown>
+
+        <Image src={picture} alt="Bau" width={200} height={200} />
       </article>
     );
   }
+}
+
+export async function getStaticProps() {
+  const data = await import('_data/home.json');
+
+  return {
+    props: {
+      data: JSON.stringify(data),
+    },
+  };
 }
