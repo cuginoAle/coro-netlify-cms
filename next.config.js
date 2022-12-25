69ffecb6ba3a1a31dd523736087e410672466a8a
cuginoAle <alessio.carnevale@gmail.com>
2022-12-25 11:26:56 +0100

    fixing image loader

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
   webpack: (cfg) => {
     cfg.module.rules.push({
       test: /\.md$/,
