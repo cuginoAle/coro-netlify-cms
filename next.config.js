f42c219e93fa5d20b685f98fe8ec27530299bd77
cuginoAle <alessio.carnevale@gmail.com>
2022-12-25 11:37:10 +0100

    wip

--- next.config.js
+++ next.config.js
@@ -1,19 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    loader: 'akamai',
-    path: '/',
-  },
-  webpack: (cfg) => {
-    cfg.module.rules.push({
-      test: /\.md$/,
-      loader: 'frontmatter-markdown-loader',
-      options: { mode: ['react-component'] },
-    });
-    return cfg;
-  },
-};
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+const nextConfig = withPlugins([
+  [
+    optimizedImages,
+    {
+      reactStrictMode: true,
+      swcMinify: true,
+      webpack: (cfg) => {
+        cfg.module.rules.push({
+          test: /\.md$/,
+          loader: 'frontmatter-markdown-loader',
+          options: { mode: ['react-component'] },
+        });
+        return cfg;
+      },
+    },
+  ],
+]);
 
 module.exports = nextConfig;
