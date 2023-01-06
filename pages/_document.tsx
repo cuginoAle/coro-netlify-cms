3320ca0dbd26b8790a67de2fb4b4cfe9b2ae17a0
cuginoAle <alessio.carnevale@gmail.com>
2023-01-06 22:35:37 +0100

    wip

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-// eslint-disable-next-line @next/next/no-script-in-document
 import Script from 'next/script';
 
 export default function Document() {
