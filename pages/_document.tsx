ad6a20bb13eef27002b6d1e3adde490fb6870a1a
cuginoAle <alessio.carnevale@gmail.com>
2023-01-06 11:46:12 +0100

    wip

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+// eslint-disable-next-line @next/next/no-script-in-document
 import Script from 'next/script';
 
 export default function Document() {
@@ -8,7 +9,10 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></Script>
+        <Script
+          src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+          strategy="lazyOnload"
+        ></Script>
       </body>
     </Html>
   );
