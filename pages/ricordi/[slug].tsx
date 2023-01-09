3a304b89a2242e44dca0f72202018b8d931d9ea2
cuginoAle <alessio.carnevale@gmail.com>
2023-01-09 01:02:56 +0100

    wipp

--- pages/ricordi/[slug].tsx
+++ pages/ricordi/[slug].tsx
@@ -1,11 +1,14 @@
+/* eslint-disable @next/next/no-img-element */
 import { GetStaticProps } from 'next';
 import { getFile } from 'helpers/getFile';
 import Head from 'next/head';
 import Layout from 'pages/layout';
 import { GlobalProps, RicordiProps } from 'types';
 import { getCollection } from 'helpers/getCollection';
-import Image from 'next/image';
+
 import ReactMarkdown from 'react-markdown';
+import style from './style.module.css';
+import { Heading } from 'components/heading';
 
 const Ricordi = (props: { settings: GlobalProps; ricordo: RicordiProps }) => {
   const { settings, ricordo } = props;
@@ -16,21 +19,21 @@ const Ricordi = (props: { settings: GlobalProps; ricordo: RicordiProps }) => {
       </Head>
 
       <Layout {...settings}>
-        <section className="p-8 text-center m-4 md:m-8">
-          <h2 className="text-3xl">{ricordo.title}</h2>
+        <section className="flex flex-col m-2 md:m-4">
+          <Heading>{ricordo.title}</Heading>
+
           <ReactMarkdown className="MD">
             {ricordo.description as string}
           </ReactMarkdown>
           <div className="flex gap-4 snap-x snap-mandatory w-full overflow-auto">
             {ricordo.foto.map((foto) => {
               return (
-                <Image
-                  className="snap-center"
+                <img
+                  className={`snap-center ${style.picture}`}
                   key={foto.image}
                   src={foto.image}
                   alt={ricordo.title}
-                  width="900"
-                  height="900"
+                  loading="lazy"
                 />
               );
             })}
