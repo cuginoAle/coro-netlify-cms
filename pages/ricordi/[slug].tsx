bc75846a76c4abfed4ca0c96b3fa1b5bf2f80293
cuginoAle <alessio.carnevale@gmail.com>
2023-01-09 18:00:08 +0100

    wip

--- pages/ricordi/[slug].tsx
+++ pages/ricordi/[slug].tsx
@@ -68,13 +68,13 @@ const Ricordi = (props: {
           </ReactMarkdown>
 
           <Heading>Altri ricordi</Heading>
-          <div className="flex gap-2">
+          <div className="flex gap-2 flex-wrap">
             {altriRicordi.map((ricordo) => {
               return (
                 <Link
                   href={`/ricordi/${ricordo}`}
                   key={ricordo}
-                  className="bg-white/20 px-4 py-2"
+                  className="bg-white/20 px-4 py-2 shrink-0"
                 >
                   <h4>{ricordo}</h4>
                 </Link>
