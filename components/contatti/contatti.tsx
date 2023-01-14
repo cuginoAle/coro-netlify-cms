2db3ae157396eaeb7165471c7bc2e109d2d02b2c
cuginoAle <alessio.carnevale@gmail.com>
2023-01-14 10:40:41 +0100

    fix

--- components/contatti/contatti.tsx
+++ components/contatti/contatti.tsx
@@ -1,11 +1,11 @@
 import { ContattoProps } from 'types';
 
 interface ContattiPageProps {
-  contatti: { membri: ContattoProps[] };
+  membri: ContattoProps[];
 }
 
-const Contatti = ({ contatti }: ContattiPageProps) => {
-  const { membri } = contatti;
+const Contatti = (props: ContattiPageProps) => {
+  const { membri } = props;
 
   return (
     <div className="overflow-auto w-full">
@@ -13,7 +13,7 @@ const Contatti = ({ contatti }: ContattiPageProps) => {
         <tbody>
           {membri.map((membro) => (
             <tr
-              key={membro.nome}
+              key={`${membro.nome}${membro.cognome}${membro.ruolo}`}
               className="whitespace-nowrap last:border-none  border-b border-white/30"
             >
               <td className="px-4 py-2">{membro.nome}</td>
