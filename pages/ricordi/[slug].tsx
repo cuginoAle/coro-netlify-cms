76cb0d5ea9ff36b770314428b4c598ed804e7290
cuginoAle <alessio.carnevale@gmail.com>
2023-02-03 09:33:16 +0100

    fixing style

--- pages/ricordi/[slug].tsx
+++ pages/ricordi/[slug].tsx
@@ -74,7 +74,7 @@ const Ricordi = (props: {
                 <Link
                   href={`/ricordi/${ricordo}`}
                   key={ricordo}
-                  className="bg-white/20 px-4 py-2 shrink-0 w-full"
+                  className="bg-white/20 px-4 py-2 shrink-0 max-w-full"
                 >
                   <h4>{ricordo}</h4>
                 </Link>
